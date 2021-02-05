f331905565953c7cd4f67a7284a3dda52c5ac1d7
Sergey Koptev <s.koptev@corp.nekki.ru>
2021-02-05 04:05:40 +0300

    Cameras handling; unlit and legacy objects rendering

---
+++ Assets/CRPipeline/Runtime/CameraRenderer.Editor.cs
@@ -0,0 +1,71 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Profiling;
+using UnityEngine.Rendering;
+
+namespace CRP
+{
+    partial class CameraRenderer
+    {
+#if UNITY_EDITOR
+        private static Material _errorMaterial;
+        
+        private string ProfilingSampleName { get; set; }
+
+        partial class ShaderTags
+        {
+            public static readonly ShaderTagId[] Legacy =
+            {
+                new ShaderTagId("Always"),
+                new ShaderTagId("ForwardBase"),
+                new ShaderTagId("PrepassBase"),
+                new ShaderTagId("Vertex"),
+                new ShaderTagId("VertexLMRGBM"),
+                new ShaderTagId("VertexLM"), 
+            };
+        }
+
+        partial void PrepareCommandBuffer()
+        {
+            Profiler.BeginSample("PrepareCommandBuffer");
+            _cmd.name = ProfilingSampleName = _camera.name;        // access to _camera.name allocates 100 bytes; cache it
+            Profiler.EndSample();
+        }
+        
+        partial void PrepareForSceneWindow()
+        {
+            if (_camera.cameraType == CameraType.SceneView)
+                ScriptableRenderContext.EmitWorldGeometryForSceneView(_camera);
+        }
+        
+        partial void DrawUnsupportedShaders()
+        {
+            if (_errorMaterial == null)
+                _errorMaterial = new Material(Shader.Find("Hidden/InternalErrorShader"));
+            
+            var drawingSettings = new DrawingSettings(ShaderTags.Legacy[0], new SortingSettings(_camera))
+            {
+                overrideMaterial = _errorMaterial
+            };
+            
+            for (int i = 1; i < ShaderTags.Legacy.Length; ++i)
+                drawingSettings.SetShaderPassName(i, ShaderTags.Legacy[i]);
+            
+            var filteringSettings = FilteringSettings.defaultValue;
+            
+            _context.DrawRenderers(_cullingResults, ref drawingSettings, ref filteringSettings);
+        }
+
+        partial void DrawGizmos()
+        {
+            if (Handles.ShouldRenderGizmos())
+            {
+                _context.DrawGizmos(_camera, GizmoSubset.PreImageEffects);
+                _context.DrawGizmos(_camera, GizmoSubset.PostImageEffects);
+            }
+        }
+#else
+    const string ProfilingSampleName = "Camera command buffer";
+#endif
+    }
+}
--- Assets/CRPipeline/Runtime/CameraRenderer.cs
+++ Assets/CRPipeline/Runtime/CameraRenderer.cs
@@ -3,34 +3,114 @@ using UnityEngine.Rendering;
 
 namespace CRP
 {
-    public class CameraRenderer
+    public partial class CameraRenderer
     {
         private ScriptableRenderContext _context;
-        private Camera _camera;
+        private Camera                  _camera;
+        private CommandBuffer           _cmd = new CommandBuffer();
+        private CullingResults          _cullingResults;
+
+        private static class ProfilingSampleNames
+        {
+            public const string ClearRT = "Clear RT";
+        }
+
+        private static partial class ShaderTags
+        {
+            public static readonly ShaderTagId Unlit = new ShaderTagId("SRPDefaultUnlit");
+        }
 
         public void Render(ScriptableRenderContext context, Camera camera)
         {
             _context = context;
             _camera = camera;
 
+            PrepareCommandBuffer();
+            PrepareForSceneWindow();
+
+            if (!Cull())
+                return;
+
             SetupCameraProperties();
             DrawVisibleGeometry();
+            DrawUnsupportedShaders();
+            DrawGizmos();
+
             Submit();
+        }
+
+        private void ExecuteBuffer()
+        {
+            _context.ExecuteCommandBuffer(_cmd);
+            _cmd.Clear();
         }
 
         private void SetupCameraProperties()
         {
-            _context.SetupCameraProperties(_camera);
+            _context.SetupCameraProperties(_camera); // before ClearRenderTarget to more effective way
+            
+            _cmd.BeginSample(ProfilingSampleNames.ClearRT);
+            {
+                var flags = _camera.clearFlags;
+                
+                _cmd.ClearRenderTarget(
+                    flags != CameraClearFlags.Nothing, 
+                    flags == CameraClearFlags.Color, 
+                    flags == CameraClearFlags.Color ? _camera.backgroundColor.linear : Color.clear);
+            }
+            _cmd.EndSample(ProfilingSampleNames.ClearRT);
+            
+            _cmd.BeginSample(ProfilingSampleName);
+            ExecuteBuffer();
         }
 
         private void DrawVisibleGeometry()
         {
+            var sortingSettings = new SortingSettings(_camera);
+            
+            //-- draw opaque unlit objects
+            {
+                sortingSettings.criteria = SortingCriteria.CommonOpaque;
+                var drawingSettings   = new DrawingSettings(ShaderTags.Unlit, sortingSettings);
+                var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+
+                _context.DrawRenderers(_cullingResults, ref drawingSettings, ref filteringSettings);
+            }
+
             _context.DrawSkybox(_camera);
+
+            //-- draw transparent unlit objects
+            {
+                sortingSettings.criteria = SortingCriteria.CommonTransparent;
+                var drawingSettings   = new DrawingSettings(ShaderTags.Unlit, sortingSettings);
+                var filteringSettings = new FilteringSettings(RenderQueueRange.transparent);
+                
+                _context.DrawRenderers(_cullingResults, ref drawingSettings, ref filteringSettings);
+            }
         }
 
         private void Submit()
         {
+            _cmd.EndSample(ProfilingSampleName);
+            ExecuteBuffer();
+            
             _context.Submit();
         }
+
+        private bool Cull()
+        {
+            if (_camera.TryGetCullingParameters(out var cullingParameters))
+            {
+                _cullingResults = _context.Cull(ref cullingParameters);
+                return true;
+            }
+
+            return false;
+        }
+
+        partial void PrepareCommandBuffer();
+        partial void PrepareForSceneWindow();
+        partial void DrawUnsupportedShaders();
+        partial void DrawGizmos();
     }
 }
