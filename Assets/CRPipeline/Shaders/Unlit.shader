df4b741969b50aaa3b6b366d0f312800eff38a47
Sergey Koptev <s.koptev@corp.nekki.ru>
2021-02-12 03:03:38 +0300

    CRP/Unlit shader: options for transparency, alpha-clipping, blend mode and z-testing; More complicated test scene

--- Assets/CRPipeline/Shaders/Unlit.shader
+++ Assets/CRPipeline/Shaders/Unlit.shader
@@ -2,19 +2,38 @@ Shader "CRP/Unlit"
 {
     Properties
     {
+        _ColorTexture("Color texture", 2D) = "white" {}
         _Color("Color", Color) = (1.0, 1.0, 1.0, 1.0)
+        
+        [Toggle(USE_ALPHA_CLIPPING)] _AlphaTesting("Alpha clipping", Float) = 0
+        _Cutoff("Cutoff", Range(0.0, 1.0)) = 0.5
+        
+        [Enum(UnityEngine.Rendering.BlendMode)] _SrcBlend ("Src Blend", Float) = 1.0
+        [Enum(UnityEngine.Rendering.BlendMode)] _DstBlend ("Dst Blend", Float) = 1.0        
+        [Enum(Off, 0, On, 1)]                   _ZWrite   ("Z Write"  , Float) = 1.0
     }
     
     SubShader
     {
+        Tags 
+        {
+            "RenderType" = "Opaque"   // needed for replacement shaders
+            "Queue"      = "Geometry" // Geometry [+ 1] / AlphaTest / Transparent
+        }
+        
         Pass
         {
+            Blend [_SrcBlend] [_DstBlend]
+            ZWrite [_ZWrite]
+                
             HLSLPROGRAM
-            #pragma multi_compile_instancing            
+            #pragma multi_compile_instancing
+            #pragma shader_feature USE_ALPHA_CLIPPING
+            
             #pragma vertex   UnlitPassVertex
             #pragma fragment UnlitPassFragment
                         
-            #include "Unlit.hlsl"
+            #include "UnlitPass.hlsl"
             ENDHLSL        
         }
     }
--- Assets/CRPipeline/Shaders/UnlitPass.hlsl
+++ Assets/CRPipeline/Shaders/UnlitPass.hlsl
@@ -6,18 +6,27 @@
 struct Attributes
 {
     float3 positionOS : POSITION;
+    float2 UV         : TEXCOORD0;
     UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
 struct Varyings
 {
     float4 positionOS : SV_POSITION;
+    float2 UV         : TEXCOORD0;
     UNITY_VERTEX_INPUT_INSTANCE_ID
 };
+//---------------------------------------------------------------------------------------
+
+TEXTURE2D(_ColorTexture);
+SAMPLER(sampler_ColorTexture);
 
 UNITY_INSTANCING_BUFFER_START(UnityPerMaterial)
+    UNITY_DEFINE_INSTANCED_PROP(float4, _ColorTexture_ST)
     UNITY_DEFINE_INSTANCED_PROP(float4, _Color)
+    UNITY_DEFINE_INSTANCED_PROP(float,  _Cutoff)
 UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
+//---------------------------------------------------------------------------------------
 
 Varyings UnlitPassVertex(Attributes Input)
 {
@@ -29,13 +38,24 @@ Varyings UnlitPassVertex(Attributes Input)
     float3 positionWS = TransformObjectToWorld(Input.positionOS);    
     Output.positionOS = TransformWorldToHClip(positionWS);
     
+    float4 uv_ST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _ColorTexture_ST);
+    Output.UV = Input.UV * uv_ST.xy + uv_ST.zw;
+    
     return Output;    
 }
 
 float4 UnlitPassFragment(Varyings Input) : SV_TARGET
 {
     UNITY_SETUP_INSTANCE_ID(Input);
-    return UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Color);
+    
+    float4 color = SAMPLE_TEXTURE2D(_ColorTexture, sampler_ColorTexture, Input.UV);
+    color *= UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Color);
+    
+    #ifdef USE_ALPHA_CLIPPING
+    clip(color.a - UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Cutoff));
+    #endif
+    
+    return color;
 }
 
 #endif
